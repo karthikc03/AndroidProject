6bf335756fe45defe9b9ac7cfb81eb3aea4b3aab
karthikc03 <karthikchandra03@gmail.com>
2017-03-29 21:45:47 -0400

    Saving data on orientation changes

---
+++ SaveInstanceState/app/src/main/java/com/example/karth/saveinstancestate/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.karth.saveinstancestate;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        count = 0;
+       textView.setText("Count: " + count);
+
+    }
+
+    public void increment(View view) {
+
+        count++;
+        textView.setText("Count: " + count);
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("myCount",count);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("myCount", 0);
+        textView.setText("Count: "+count);
+    }
+}
